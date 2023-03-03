8c79bda68261af7ba526638899698216b6e16bfb
Jopa <joaopaulocosta.r@hotmail.com>
2023-03-03 16:50:23 -0300

    Anotações importantes

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,29 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+    //tipos primitivos importantes
+    //estudar tudo ao redor de String
+
+    /*int e log = números inteiros
+     * exemplo:
+     * int = 5556
+     * long = 2134535435356L
+     * 
+     * 
+     * double e float = números fracionados
+     * exemplo:
+     * double = 3.145
+     * float = 3.14F
+     * 
+     * 
+     * float e long (necessário por F e L ao final do número)
+     */
+
+    /*Constates são valores fixos e sempre escritas em caps lock, caixa alta.
+     * exemplo:
+     * final double = 3.14
+     * o termo "final" determina que um valor é fixo
+     */
+
     }
 }
