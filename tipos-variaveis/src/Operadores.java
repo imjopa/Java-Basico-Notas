0de9cb920150cf84422943d3c321f8480d847622
Jopa <joaopaulocosta.r@hotmail.com>
2023-03-03 17:11:39 -0300

    Anotações de Operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,39 @@
+public class Operadores {
+    public static void main(String[] args) {
+
+        /*
+         * Atribuição: Representado pelo símbolo de igualdade =.
+         * exemplos:
+         * String nome = "GLEYSON";
+         * int idade = 22;
+         * double peso = 68.5;
+         * char sexo = 'M';
+         * boolean doadorOrgao = false;
+         * Date dataNascimento = new Date();
+         */
+        String nome = "Johnny";
+        System.out.print(nome);
+
+        /*
+         * Aritméticos
+         * * Os operadores aritméticos são: + (adição), - (subtração), * (multiplicação)
+         * e / (divisão).
+         * double soma = 10.5 + 15.7;
+         * int subtração = 113 - 25;
+         * int multiplicacao = 20 * 7;
+         * int divisao = 15 / 3;
+         * int modulo = 18 % 3;
+         * double resultado = (10 * 7) + (20/4);
+         */
+
+         double multiplicacao = 10.5 * 5;
+         System.out.print(multiplicacao);
+         // ou 
+         String nomeCompleto = "Johnny" + "James";
+         System.out.print(nomeCompleto);
+
+
+         // mais infomações em https://felipe-silva-aguiar.gitbook.io/dio-java/gitbook/sintaxe/operadores
+
+    }
+}
